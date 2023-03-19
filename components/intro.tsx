659ad41716836aa74414d69cfd946f7619e7d06c
Christian Westgaard <Christian@Wests-Air.local>
2023-03-19 08:28:17 +0100

    oppdatert til paaskerebus

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Påskequiz 2023 🎉🐥
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
@@ -14,7 +14,7 @@ const Intro = () => {
         >
           Next.js
         </a>{' '}
-        and {CMS_NAME}.
+        and {CMS_NAME}.🐣
       </h4>
     </section>
   )
